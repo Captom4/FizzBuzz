116e6f4493c7bea12cbd8c1153d52b08802ae454
Tom Hird <tombjh@hotmail.co.uk>
2018-09-26 18:30:40 +0000

    Tests work

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,7 @@
-def fizzbuzz(number)
-    1.upto(number) do |n|
-      print "Fizz" if fizz = (n % 3) == 0
-      print "Buzz" if buzz = (n % 5) == 0
-      print n unless (fizz || buzz)
-      puts
-    end
+def fizzbuzz(n)
+  answer = ""
+  answer += "fizz" if fizz = (n % 3) == 0
+  answer += "buzz" if buzz = (n % 5) == 0
+  answer = n unless (fizz || buzz)
+  return answer
 end

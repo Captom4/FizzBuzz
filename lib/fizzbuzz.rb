c061b490db16dff82391ff8d00426a241b788c4c
Tom Hird <tombjh@hotmail.co.uk>
2018-09-26 15:27:57 +0000

    FizzBuzz added to lib

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(number)
-  if ((number % 3 == 0) and (number % 5 != 0))
-    puts "Fizz"
-  elsif ((number % 3 != 0) and (number % 5 == 0))
-    puts "Buzz"
-  elsif ((number % 3 == 0) and (number % 5 == 0))
-    puts "FizzBuzz"
-  else
-    puts number
-  end    
+fizzbuzz =
+1.upto(20).map do |i|
+  case
+  when i % 3 == 0 && i % 5 == 0 then "FizzBuzz"
+  when i % 3 == 0 then "Fizz"
+  when i % 5 == 0 then "Buzz"
+  else i
+  end
 end
+
+puts fizzbuzz

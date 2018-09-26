558e7c8457160fa4efd82c971d79956189aa4761
Tom Hird <tombjh@hotmail.co.uk>
2018-09-26 17:54:06 +0000

    completed libs

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,8 @@
-fizzbuzz =
-1.upto(20).map do |i|
-  case
-  when i % 3 == 0 && i % 5 == 0 then "FizzBuzz"
-  when i % 3 == 0 then "Fizz"
-  when i % 5 == 0 then "Buzz"
-  else i
-  end
+def fizzbuzz(number)
+    1.upto(number) do |n|
+      print "Fizz" if fizz = (n % 3) == 0
+      print "Buzz" if buzz = (n % 5) == 0
+      print n unless (fizz || buzz)
+      puts
+    end
 end
-
-puts fizzbuzz
